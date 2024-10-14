e1d8579d947eec19f309f89984573d36d04afd71
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-10-14 22:19:00 +0200

    Rework Wave System

--- Assets/Code/Scripts/Objects/EnemySpawner.cs
+++ Assets/Code/Scripts/Objects/EnemySpawner.cs
@@ -14,7 +14,6 @@ public class EnemySpawner : MonoBehaviour
     public ShopMenu shopMenu;
 
     [Header("Attributes")]
-    [SerializeField] private int baseEnemies = 8;
     [SerializeField] private float enemiesPerSecond = 5.5f;
 
     [Header("Events")]
@@ -30,6 +29,30 @@ public class EnemySpawner : MonoBehaviour
     private bool isSpawning = false;
     private bool waveTimeModified = false;
     private float timeBetweenWaves = 15f;
+
+    //0 = Ruby / 1 = Azurite / 2 = ??
+    List<List<int>> waveList = new List<List<int>>(){
+        new List<int>{0, 0, 0, 0, 0, 0, 0, 0}, //Wave 1 : 8 Enemies
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, //Wave 5 : 15 Enemies
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, //Wave 10 : 25 Enemies
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, //Wave 15 : 35 Enemies
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, //Wave 19 : 50 Enemies
+        new List<int>{0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0}, //Wave 20 : 75 Enemies
+    };
 
     private void Awake()
     {
@@ -91,7 +114,7 @@ public class EnemySpawner : MonoBehaviour
         if (currentWave != 21)
         {
             isSpawning = true;
-            enemiesLeftToSpawn = EnemiesPerWave();
+            enemiesLeftToSpawn = waveList[currentWave - 1].Count;
             eps = EnemiesPerSecond();
         }
     }
@@ -110,328 +133,12 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        if (currentWave == 1)
-        {
-            RubySpawn();
-            return;
 
-        }
-
-        if (currentWave == 2)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 3)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 4)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 5)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 6)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 7)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 8)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 9)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 10)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 11)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 12)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 13)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 14)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 15)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 16)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 17)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 18)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 19)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 20)
-        {
-            if (enemiesLeftToSpawn >= 3)
-            {
-                RubySpawn();
-                return;
-            }
-            if (enemiesLeftToSpawn <= 2)
-            {
-                AzuriteSpawn();
-                return;
-            }
-        }
-
-        if (currentWave == 21)
-        {
-            return;
-        }
-    }
-
-    private int EnemiesPerWave()
-    {
-        if (currentWave == 1) { return Mathf.RoundToInt(baseEnemies); } //baseEnemies = 8
-
-        if (currentWave == 2) { return Mathf.RoundToInt(baseEnemies + 2); }
-
-        if (currentWave == 3) { return Mathf.RoundToInt(baseEnemies + 3); }
-
-        if (currentWave == 4) { return Mathf.RoundToInt(baseEnemies + 4); }
-
-        if (currentWave == 5) { return Mathf.RoundToInt(baseEnemies + 7); } //baseEnemies + 7 = 15
-
-        if (currentWave == 6) { return Mathf.RoundToInt(baseEnemies + 8); }
-
-        if (currentWave == 7) { return Mathf.RoundToInt(baseEnemies + 9); }
-
-        if (currentWave == 8) { return Mathf.RoundToInt(baseEnemies + 10); }
-
-        if (currentWave == 9) { return Mathf.RoundToInt(baseEnemies + 12); }
-
-        if (currentWave == 10) { return Mathf.RoundToInt(baseEnemies + 17); } //baseEnemies + 17 = 25
-
-        if (currentWave == 11) { return Mathf.RoundToInt(baseEnemies + 18); }
-
-        if (currentWave == 12) { return Mathf.RoundToInt(baseEnemies + 19); }
-
-        if (currentWave == 13) { return Mathf.RoundToInt(baseEnemies + 20); }
-
-        if (currentWave == 14) { return Mathf.RoundToInt(baseEnemies + 22); }
-
-        if (currentWave == 15) { return Mathf.RoundToInt(baseEnemies + 27); } //baseEnemies + 27 = 35
-
-        if (currentWave == 16) { return Mathf.RoundToInt(baseEnemies + 29); }
-
-        if (currentWave == 17) { return Mathf.RoundToInt(baseEnemies + 32); }
-
-        if (currentWave == 18) { return Mathf.RoundToInt(baseEnemies + 37); }
-
-        if (currentWave == 19) { return Mathf.RoundToInt(baseEnemies + 42); } //baseEnemies + 42 = 50
-
-        if (currentWave == 20) { return Mathf.RoundToInt(baseEnemies + 67); } //baseEnemies + 67 = 75
-
-        return Mathf.RoundToInt(baseEnemies + 6);
+        List<int> selectedList = waveList[currentWave - 1];
+        int index = waveList[currentWave - 1][0];
+        waveList[currentWave - 1].RemoveAt(0);
+        GameObject prefabToSpawn = enemyPrefabs[index];
+        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
     }
 
     private float EnemiesPerSecond()
@@ -469,19 +176,5 @@ public class EnemySpawner : MonoBehaviour
         if (currentWave == 21) { return enemiesPerSecond = enemiesPerSecond + 2.2f; }
 
         return enemiesPerSecond = enemiesPerSecond + 0.5f;
-    }
-
-    private void RubySpawn()
-    {
-        int index = 0;
-        GameObject prefabToSpawn = enemyPrefabs[index];
-        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
-    }
-
-    private void AzuriteSpawn()
-    {
-        int index = 1;
-        GameObject prefabToSpawn = enemyPrefabs[index];
-        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
     }
 }
